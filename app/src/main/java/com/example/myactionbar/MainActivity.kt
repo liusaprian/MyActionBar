eafdd9881d3554ebfe89a74e6ec05c5c9656b067
liusaprian <aprianlius@gmail.com>
2020-08-09 14:18:42 +0700

    MyActionBar

---
+++ app/src/main/java/com/example/myactionbar/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.myactionbar
+
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search_bar).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String): Boolean {
+                Toast.makeText(this@MainActivity, p0, Toast.LENGTH_SHORT).show()
+                return true
+            }
+            override fun onQueryTextChange(p0: String): Boolean {
+                return false
+            }
+
+        })
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.menu1 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, MenuFragment())
+                    .addToBackStack(null)
+                    .commit()
+                true
+            }
+            R.id.menu2 -> {
+                startActivity(Intent(this@MainActivity, MenuActivity::class.java))
+                true
+            }
+            else -> true
+        }
+    }
+}
